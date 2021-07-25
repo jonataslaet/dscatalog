26d608c6d758c02c257525869c999b4c8811369f
Jonatas Laet <jonataslaetprogramador@gmail.com>
2021-07-25 14:54:54 -0300

    Refactor pageable

--- backend/src/main/java/com/devsuperior/dscatalog/services/ClientService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/ClientService.java
@@ -4,7 +4,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,8 +20,8 @@ public class ClientService {
 	private ClientRepository clientRepository;
 
 	@Transactional(readOnly=true)
-	public Page<ClientDTO> findAllPaged(PageRequest pageRequest) {
-		Page<Client> clientsPaged = clientRepository.findAll(pageRequest);
+	public Page<ClientDTO> findAllPaged(Pageable pageable) {
+		Page<Client> clientsPaged = clientRepository.findAll(pageable);
 		return clientsPaged.map(c -> new ClientDTO(c));
 	}
 	
--- backend/src/main/java/com/devsuperior/dscatalog/services/ProductService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/ProductService.java
@@ -4,7 +4,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -26,8 +26,8 @@ public class ProductService {
 	private CategoryRepository categoryRepository;
 	
 	@Transactional(readOnly=true)
-	public Page<ProductDTO> findAllPaged(PageRequest pageRequest) {
-		Page<Product> productsPaged = productRepository.findAll(pageRequest);
+	public Page<ProductDTO> findAllPaged(Pageable pageable) {
+		Page<Product> productsPaged = productRepository.findAll(pageable);
 		return productsPaged.map(c -> new ProductDTO(c));
 	}
 	
