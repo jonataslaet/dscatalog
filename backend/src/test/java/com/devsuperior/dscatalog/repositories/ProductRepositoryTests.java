ae3d544febf916c7570e0f081ecb3795cc198abc
Jonatas Laet <jonataslaetprogramador@gmail.com>
2021-07-30 00:35:43 -0300

    Refactor ProductRepositoryTests

--- backend/src/test/java/com/devsuperior/dscatalog/repositories/ProductRepositoryTests.java
+++ backend/src/test/java/com/devsuperior/dscatalog/repositories/ProductRepositoryTests.java
@@ -3,12 +3,13 @@ package com.devsuperior.dscatalog.repositories;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.EmptyResultDataAccessException;
 
 import com.devsuperior.dscatalog.entities.Product;
-import com.devsuperior.dscatalog.resources.exceptions.EntityNotFoundException;
 
 @DataJpaTest
 class ProductRepositoryTests {
@@ -16,26 +17,27 @@ class ProductRepositoryTests {
 	@Autowired
 	private ProductRepository productRepository;
 	
+	private Long existingId;
+	private Long nonExistingId;
+	
+	@BeforeEach
+	void setUp() {
+		existingId = 1L;
+		nonExistingId = 9999999L;
+	}
+	
 	@Test
 	void deleteShouldDeleteObjectWhenIdExists() {
-		Long existingId = 1L;
-		
 		productRepository.deleteById(existingId);
-		
 		Optional<Product> productFound = productRepository.findById(existingId);
-		
 		Assertions.assertFalse(productFound.isPresent());
 	}
 
 	@Test
-	void deleteShouldThrowEntityNotFoundExceptionWhenIdDoesNotExist() {
-		Long nonExistingId = 9999999L;
-		
+	void deleteShouldThrowEmptyResultDataAccessExceptionWhenIdDoesNotExist() {
 		Optional<Product> productFound = productRepository.findById(nonExistingId);
-		
 		Assertions.assertFalse(productFound.isPresent());
-		
-		Assertions.assertThrows(EntityNotFoundException.class, ()->{
+		Assertions.assertThrows(EmptyResultDataAccessException.class, ()->{
 			productRepository.deleteById(nonExistingId);
 		});
 	}
