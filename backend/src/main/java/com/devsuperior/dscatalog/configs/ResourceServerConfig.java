7319ba627a11bb154fb65141e888bd49eb9f4148
Jonatas Laet <jonataslaetprogramador@gmail.com>
2021-08-16 23:31:31 -0300

    Configuring h2-console endpoint on ResourceServerConfig

--- backend/src/main/java/com/devsuperior/dscatalog/configs/ResourceServerConfig.java
+++ backend/src/main/java/com/devsuperior/dscatalog/configs/ResourceServerConfig.java
@@ -1,7 +1,10 @@
 package com.devsuperior.dscatalog.configs;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -16,7 +19,10 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
 	@Autowired
 	private JwtTokenStore jwtTokenStore;
 	
-	public static String[] PUBLIC = {"/oauth/token"};
+	@Autowired
+	private Environment env;
+	
+	public static String[] PUBLIC = {"/oauth/token", "/h2-console/**"};
 	
 	public static String[] OPERATOR_OR_ADMIN = {"/products/**", "/categories/**"};
 	
@@ -24,6 +30,11 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
 	
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
+		
+		if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
+			http.headers().frameOptions().disable();
+		}
+		
 		http.authorizeRequests()
 			.antMatchers(PUBLIC).permitAll()
 			.antMatchers(HttpMethod.GET, OPERATOR_OR_ADMIN).permitAll()
